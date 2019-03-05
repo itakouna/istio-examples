b7b9105c0b81b675f82c9a0542a71f2c126d1f89
Ibrahim takouna <ibrahim.takouna@deliveryhero.com>
2019-03-05 11:45:49 +0100

    Update booking service

--- microservices/bookings/bookings.py
+++ microservices/bookings/bookings.py
@@ -6,34 +6,51 @@ from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
+service_version = "bookings-v1"
+
 with open("{}/bookings.json".format(root_dir()), "r") as f:
     bookings = json.load(f)
+
 
 @app.route("/health", methods=['GET'])
 def health():
     return "I'm alive"
 
+
 @app.route("/", methods=['GET'])
 def hello():
-    return nice_json({
-        "uri": "/",
-        "subresource_uris": {
-            "bookings": "/bookings",
-            "booking": "/bookings/<username>"
+    response = {
+        "service": service_version,
+        "data": {
+            "uri": "/",
+            "subresource_uris": {
+                "bookings": "/bookings",
+                "booking": "/bookings/<username>"
+            }
         }
-    })
+    }
+    return nice_json(response)
+
 
 @app.route("/bookings", methods=['GET'])
 def booking_list():
-    return nice_json(bookings)
+    response = {
+        "service": service_version,
+        "data": bookings
+    }
+    return nice_json(response)
 
 
 @app.route("/bookings/<username>", methods=['GET'])
 def booking_record(username):
     if username not in bookings:
         raise NotFound
+    response = {
+        "service": service_version,
+        "data": bookings[username]
+    }
+    return nice_json(response)
 
-    return nice_json(bookings[username])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
